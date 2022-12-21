c097e743b0f64f83cfd43226f88344e6ad495926
Reginald Green <reggie.green10@yahoo.com>
2022-12-21 17:35:25 -0500

    "bio practice "

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,8 +22,8 @@ public class Main {
         // grade (character)
         char grade = 'A';
 
-        System.out.println("My name is "+ name + ". I'm " + age +" years old, and I'm from" +country+ "." );
-        System.out.println("My favourite sport is" +sport+ ". I play for " +hours+ " hours a day");
+        System.out.println("My name is "+ name + ". I'm " + age +" years old, and I'm from the " +country+ "." );
+        System.out.println("My favourite sport is " +sport+ ". I play for " +hours+ " hours a day.");
         System.out.println("When I'm tired, I like to play " +game+ ".");
         System.out.println("In school, my favourite subject was " +subject+ ", I scored a " +grade+ ".");
 
